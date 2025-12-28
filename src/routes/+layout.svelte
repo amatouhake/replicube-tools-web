a47f5ed83db4525e6db8e841cb266f8e34b1b434
amatouhake <kenkenhagizou@gmail.com>
2025-12-28 18:22:42 +0900

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import 'carbon-components-svelte/css/all.css';
+	import { Theme } from 'carbon-components-svelte';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<Theme theme="g100" render="toggle" />
